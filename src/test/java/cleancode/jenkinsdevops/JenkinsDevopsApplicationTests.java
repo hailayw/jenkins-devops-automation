f398e6ca7b28533b2cd0f16cbbb4264070d55130
hailayw <hailaygidey5@gmail.com>
2022-10-18 11:09:13 -0500

    first commit

---
+++ src/test/java/cleancode/jenkinsdevops/JenkinsDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package cleancode.jenkinsdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
